b084488b062fb5d556bc2ecc497c51a3fb0bde8a
deepu-paswan-1123 <159120399+deepu-paswan-1123@users.noreply.github.com>
2024-07-10 10:00:12 +0530

    calculator code push

---
+++ calculator/src/component/ButtonsContainer.jsx
@@ -0,0 +1,16 @@
+import style from './ButtonsContainer.module.css'
+const ButtonContainer = ({OnButtonClick}) => {
+    let buttonsName=["C","1","2","3","+","4","5","6","-","7","8","9","*","=","/","0",".","%"]
+  return (
+    <div className={style.buttonContainer}>
+        {
+            buttonsName.map((ButtonName)=>
+                <button  className={style.button} onClick={()=> OnButtonClick(ButtonName)}>{ButtonName}</button>
+            )
+        }
+    
+    </div>
+  );
+};
+
+export default ButtonContainer;
