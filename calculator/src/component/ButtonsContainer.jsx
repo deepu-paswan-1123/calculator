2b2b5bec0e3e11f4b860c352879342bcf42d4660
deepu-paswan-1123 <159120399+deepu-paswan-1123@users.noreply.github.com>
2024-07-10 10:05:49 +0530

    only btn component

--- calculator/src/component/ButtonsContainer.jsx
+++ calculator/src/component/ButtonsContainer.jsx
@@ -1,3 +1,4 @@
+// hhh
 import style from './ButtonsContainer.module.css'
 const ButtonContainer = ({OnButtonClick}) => {
     let buttonsName=["C","1","2","3","+","4","5","6","-","7","8","9","*","=","/","0",".","%"]
